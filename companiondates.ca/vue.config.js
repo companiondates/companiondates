cb8f4df33974d0c461473c269c6c22fbc0b4896c
Nick <delunico@sheridancollege.ca>
2022-01-07 01:28:57 -0500

    added ecommerce template

---
+++ companiondates.ca/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    runtimeCompiler: true,
+    devServer: {
+        proxy: {
+            "^/api": {
+                target: "http://localhost:3000/",
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' }
+            }
+        }
+    }
+};
