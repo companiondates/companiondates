702ed1d720ce9ffe9a3eaf73400fada38395be83
Nick <delunico@sheridancollege.ca>
2022-10-09 00:03:32 -0400

    removing money back guarantee

--- companiondates.ca/src/components/Home.vue
+++ companiondates.ca/src/components/Home.vue
@@ -379,21 +379,10 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-card  class="accent" >
+    <v-card class="accent" >
       <v-container>
-        <v-row no-gutters>
-          <v-col class="col-12 col-md-4 col-sm-12">
-            <v-row >
-              <v-col class="col-12 col-sm-3 pr-4" align="right">
-                <v-icon class="display-2">mdi--currency-usd</v-icon>
-              </v-col>
-              <v-col  class="col-12 col-sm-9 pr-4">
-                <h3 class="font-weight-light">MONEY BACK GUARANTEE</h3>
-                <p class="font-weight-thin">30 Days Money Back Guarantee</p>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col class="col-12 col-md-4 col-sm-12">
+        <v-row>
+          <v-col class="col-12 col-md-4 col-sm-12 mx-auto">
             <v-row>
               <v-col class="col-12 col-sm-3 pr-4" align="right">
                 <v-icon class="display-2">mdi-headset</v-icon>
